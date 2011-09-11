f9abc4309108d0012424d03032a97534bab520a0
Brett Reid <baronblu@gmail.com>
2011-09-11 14:11:53 -0400

    added license notice to file I added.

--- src/wild4.c
+++ src/wild4.c
@@ -5,9 +5,16 @@
 /*
  * Copyright (c) 2008 Brett Reid
  *
- * This software may be copied and distributed for educational, research, and
- * not for profit purposes provided that this copyright and statement are
- * included in all such copies.
+ * This work is free software; you can redistribute it and/or modify it
+ * under the terms of either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation, version 2, or
+ *
+ * b) the "Angband license":
+ *    This software may be copied and distributed for educational, research,
+ *    and not for profit purposes provided that this copyright and statement
+ *    are included in all such copies.  Other copyrights may also apply.
  */
 #include "angband.h"
 
