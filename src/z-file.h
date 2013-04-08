c15340710cba8942dc5078ba06eb8b5895ce4dc5
Brett Reid <baronblu@gmail.com>
2013-04-08 15:14:59 -0400

    start changing file handling to match angband.

--- src/z-file.h
+++ src/z-file.h
@@ -4,14 +4,236 @@
 #ifndef INCLUDED_Z_FILE_H
 #define INCLUDED_Z_FILE_H
 
+/*** File access code ***/
+
+/** Data types **/
+
+/**
+ * An opaque file handle for Angband file handling.
+ */
+typedef struct ang_file ang_file;
+
+/**
+ * Specifies what kind of access is required to a file.  See file_open().
+ */
+typedef enum
+{
+	MODE_WRITE,
+	MODE_READ,
+	MODE_APPEND
+} file_mode;
+
+/**
+ * Specifies what kind of thing a file is, when writing.  See file_open().
+ */
+typedef enum
+{
+	FTYPE_TEXT = 1,
+	FTYPE_SAVE,
+	FTYPE_RAW,
+	FTYPE_HTML
+} file_type;
+
+/** Utility functions **/
+
+/**
+ * Returns TRUE if `fname` exists (and is a file), FALSE otherwise.
+ */
+bool file_exists(const char *fname);
+
+/**
+ * Tries to delete `fname`.
+ *
+ * Returns TRUE if successful, FALSE otherwise.
+ */
+bool file_delete(const char *fname);
+
+/**
+ * Moves the file `fname` to `newname`.
+ *
+ * Returns TRUE if successful, FALSE otherwise.
+ */
+bool file_move(const char *fname, const char *newname);
+
+/**
+ * Returns TRUE if the file `first` is newer than `second`.
+ */
+bool file_newer(const char *first, const char *second);
+
+
+/** File handle creation **/
+
+/**
+ * Open file `buf`, returning a file handler representing that file.
+ *
+ * The file mode specifies what kind of access is required to the file:
+ *  - MODE_WRITE will overwrite the current contents of the file
+ *  - MODE_READ will allow read-only access to the file
+ *  - MODE_APPEND will allow write-only access, but will not overwrite the
+ *    current contents of the file.
+ *
+ * The file type is specified to allow systems which don't use file extensions
+ * to set the type of the file appropriately.  When reading, pass -1 as ftype;
+ * when writing, use whichever filetype seems most appropriate.
+ *
+ * On any kind of error, this function returns NULL.
+ */
+ang_file *file_open(const char *buf, file_mode mode, file_type ftype);
+
+
+/**
+ * Platform hook for file_open.  Used to set filetypes.
+ */
+extern void (*file_open_hook)(const char *path, file_type ftype);
+
+
+/**
+ * Attempt to close the file handle `f`.
+ *
+ * Returns TRUE if successful, FALSE otherwise.
+ */
+bool file_close(ang_file *f);
+
+
+/** File locking **/
+
+/**
+ * Lock or unlock the file represented by `f` for writing.
+ * If the file is not open for writing, this call will fail.
+ *
+ * If `f` is closed, the file is automatically unlocked.
+ */
+void file_lock(ang_file *f);
+void file_unlock(ang_file *f);
+
+
+/** Line-based IO **/
+
+/**
+ * Get a line of text from the file represented by `f`, placing it into `buf`
+ * to a maximum length of `n`.
+ *
+ * This expands tabs, replaces non-printables with '?', and deals with differing
+ * line endings.
+ *
+ * Returns TRUE when data is returned; FALSE otherwise.
+ */
+bool file_getl(ang_file *f, char *buf, size_t n);
+
+/**
+ * Write the string pointed to by `buf` to the file represented by `f`.
+ *
+ * Returns TRUE if successful, FALSE otherwise.
+ */
+bool file_put(ang_file *f, const char *buf);
+
+/**
+ * Format (using strnfmt) the given args, and then call file_put().
+ */
+bool file_putf(ang_file *f, const char *fmt, ...);
+bool file_vputf(ang_file *f, const char *fmt, va_list vp);
+
+/**
+ * Format a string, then print it out to file.
+ */
+bool x_file_putf(ang_file *f, const char *fmt, ...);
+
+
+/** Byte-based IO */
+
+/**
+ * Seek to position `pos` in the file represented by `f`.
+ *
+ * Returns TRUE if successful, FALSE otherwise.
+ */
+bool file_seek(ang_file *f, u32b pos);
+
+/**
+ * Reads n bytes from file 'f' into buffer 'buf'.
+ * \returns Number of bytes read; -1 on error
+ */
+int file_read(ang_file *f, char *buf, size_t n);
+
+/**
+ * Write the first `n` bytes following the pointer `buf` to the file represented
+ * by `f`.  Do not mix with calls to file_writec().
+ *
+ * Returns TRUE if successful, FALSE otherwise.
+ */
+bool file_write(ang_file *f, const char *buf, size_t n);
+
+/**
+ * Read a byte from the file represented by `f` and place it at the location
+ * specified by 'b'.
+ *
+ * Returns TRUE if successful, FALSE otherwise.
+ */
+bool file_readc(ang_file *f, byte *b);
+
+/**
+ * Write the byte `b` to the file represented by `f`.
+ *
+ * Returns TRUE if successful, FALSE otherwise.
+ */
+bool file_writec(ang_file *f, byte b);
+
+
+
+/*** Directory code ***/
+
+/**
+ * Return whether or not a directory exists.
+ */
+bool dir_exists(const char *dirname);
+
+/**
+ * Create's the given directory, creating intermediate directories if
+ * needed and possible. Returns whether or not the directory was created
+ * successfully.
+ */
+bool dir_create(const char *dirname);
+
+/**
+ * An opaque file handle for Angband directory handling.
+ */
+typedef struct ang_dir ang_dir;
+
+/**
+ * Opens a directory handle.
+ *
+ * `dirname` must be a system-specific pathname to the directory
+ * you want scanned.
+ *
+ * Returns a valid directory handle on success, NULL otherwise.
+ */
+ang_dir *dir_open(const char *dirname);
+
+/**
+ * Reads a directory entry.
+ *
+ * `dir` must point to a directory handle previously returned by my_dopen().
+ * `fname` must be a pointer to a writeable chunk of memory `len` long.
+ *
+ * Returns TRUE on successful reading, FALSE otherwise.
+ * (FALSE generally indicates that there are no more files to be read.)
+ */
+bool dir_read(ang_dir *dir, char *fname, size_t len);
+
+/**
+ * Close a directory handle.
+ */
+void dir_close(ang_dir *dir);
+
 /* z-file.c */
 extern errr path_parse(char *buf, int max, cptr file);
 extern void path_build(char *buf, int max, cptr path, cptr file);
+
 extern FILE *my_fopen(cptr file, cptr mode);
 extern FILE *my_fopen_temp(char *buf, int max);
 extern errr my_fgets(FILE *fff, char *buf, huge n);
 extern errr my_raw_fgets(FILE *fff, char *buf, huge n);
 extern void my_fclose(FILE *fff);
+
 extern errr fd_kill(cptr file);
 extern errr fd_move(cptr file, cptr what);
 extern int fd_make(cptr file, int mode);
